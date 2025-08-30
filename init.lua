25f99e15ab04f33e026cdab754e8ab4a853b6637
rokonio <chachavire@ik.me>
2025-08-30 03:41:57 +0200

    remove netrw

--- init.lua
+++ init.lua
@@ -13,10 +13,10 @@ local spec = {
     end
   },
   { "derektata/lorem.nvim" },
-  { "nvim-treesitter/nvim-treesitter-context", opts = { min_window_height = 1, separator = '-' } },
+  { "nvim-treesitter/nvim-treesitter-context", opts = { separator = '-' } },
   { "nvim-treesitter/nvim-treesitter", config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = {"typst", "rust", "python", "c"},
+        ensure_installed = { "typst", "rust", "python", "c" },
         highlight = { enable = true }
       })
     end,
@@ -47,25 +47,11 @@ vim.opt.swapfile = false
 vim.opt.winborder = "rounded"
 vim.opt.scrolloff = 6
 
-vim.g.netrw_winsize = 25
-vim.g.netrw_banner = 0
-
--- There is a bug where, if I configure netrw_liststyle=3 in the config
--- happens, but not when I switch to it by using i: when I press enter on a
--- folder right after opening netrw, I cd into this folder instead of
--- unfolding it, which leads to a second bug: when pressing - into this folder
--- that is now the only open, nvim crashes. 
--- I should probably submit a bug report, and a patch if I feel brave
-vim.g.netrw_liststyle = 3
--- vim.cmd(":Vexplore") -- Use this to open the root directory (CWD)
--- vim.schedule(function() vim.cmd('silent! normal i') end)
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<space>", "<Nop>")
 vim.keymap.set({"n", "x"}, "<Leader>y", "\"+y")
 vim.keymap.set({"n", "x"}, "<Leader>Y", "<cmd>%y+<cr>")
 vim.keymap.set({"n", "x"}, "<Leader>ff", "<cmd>FzfLua files<cr>")
-vim.keymap.set({"n", "x"}, "<Leader>f", "<cmd>FzfLua<cr>")
 vim.keymap.set({"n", "x"}, "j", "gj")
 vim.keymap.set({"n", "x"}, "k", "gk")
 vim.keymap.set({"n", "x", "i"}, "<C-C>", "<cmd>!start cmd '%'<cr>")
