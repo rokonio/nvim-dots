547dea586d73e18817ae57f7c7a0052e0bea38b9
rokonio <chachavire@ik.me>
2026-02-03 15:55:52 +0100

    Update

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.pack.add({ "https://github.com/folke/lazy.nvim" })
 require("lazy").setup({
-  { "derektata/lorem.nvim" },
+  -- { "derektata/lorem.nvim" },
   { "lewis6991/gitsigns.nvim",                 opts = {} },
   { "ibhagwan/fzf-lua",                        opts = {} },
   { "folke/which-key.nvim",                    opts = {} },
@@ -9,7 +9,7 @@ require("lazy").setup({
   { "lukas-reineke/indent-blankline.nvim",     opts = {}, main = "ibl" },
   { "m4xshen/autoclose.nvim",                  opts = { options = { pair_spaces = true } } },
   { "nvim-treesitter/nvim-treesitter-context", opts = { separator = '-' } },
-  { "nvim-treesitter/nvim-treesitter",         main = "nvim-treesitter.configs",
+  { "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = { "typst", "rust", "python", "c" },
       highlight = { enable = true },
@@ -23,9 +23,10 @@ require("lazy").setup({
 
   -- LSP and more IDE like experience
   { "neovim/nvim-lspconfig" },
-  { "mason-org/mason.nvim",           opts = {} },
+  { "mason-org/mason.nvim",           opts = {}, cmd = { "Mason", "MasonInstall" } },
   { "mason-org/mason-lspconfig.nvim", opts = {} },
   { "saghen/blink.cmp", version = "v1.6.0",
+    event = "InsertEnter",
     opts = {
       keymap = { preset = "enter" },
       completion = {
@@ -45,23 +46,28 @@ require("lazy").setup({
 
   -- Language specific plugins
   { "chomosuke/typst-preview.nvim", opts = { open_cmd = "start firefox -P typst-preview %s" } }
+}, {
+  performance = {
+    cache = { enabled = true },
+  }
 })
 vim.opt.ruler = true
 vim.opt.scrolloff = 6
+vim.opt.winheight = 21
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<space>", "<Nop>")
 vim.keymap.set({ "n", "x" }, "<Leader>y", "\"+y")
+vim.keymap.set({ "n", "x" }, "<Leader>h", ":e ~\\Documents\\myhelp.md<cr>")
 vim.keymap.set({ "n", "x" }, "<Leader>Y", "<cmd>%y+<cr>")
-vim.keymap.set({ "n", "x" }, "<Leader>ff", "<cmd>FzfLua files<cr>")
+vim.keymap.set({ "n", "x" }, "<Leader><Leader>", "<cmd>FzfLua files<cr>")
 vim.keymap.set({ "n", "x" }, "j", "gj")
 vim.keymap.set({ "n", "x" }, "k", "gk")
--- vim.keymap.set({ "n", "x", "i" }, "<C-C>", "<cmd>!start cmd '%'<cr>")
+vim.keymap.set({ "n", "x", "i" }, "<C-C>", "<cmd>!start powershell<cr>")
 vim.keymap.set({ "t", }, "<C-N>", "<C-\\><C-n>")
 vim.keymap.set({ "t", }, "<C-w>", "<C-\\><C-n><C-w>")
 
-
--- TODO: Maybe put this in another file ?
+-- TODO: Maybe put this in another file
 vim.lsp.config('lua_ls', {
   settings = {
     Lua = {
